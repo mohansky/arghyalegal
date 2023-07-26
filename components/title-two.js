d922932eadb9a17eab7af2e9b2298d102d4cd81f
mohansky <38560439+mohansky@users.noreply.github.com>
2023-07-26 12:19:15 +0530

    Delete components directory

--- components/title-two.js
+++
@@ -1,9 +0,0 @@
-export default function TitleTwo({ className, children }) {
-  return (
-    <h2
-      className={`text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl mb-10 ${className}`}
-    >
-      {children}
-    </h2>
-  );
-}
