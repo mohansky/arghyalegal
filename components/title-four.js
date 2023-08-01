7d6dcd290a3b748612d346e5557bc44bbe9938df
mohansky <38560439+mohansky@users.noreply.github.com>
2023-08-01 13:19:47 +0530

    Delete components directory

--- components/title-four.js
+++
@@ -1,9 +0,0 @@
-export default function TitleFour({ className, children }) {
-  return (
-    <p
-      className={`leading-7 font-medium tracking-tight text-gray-900 mb-5 ${className}`}
-    >
-      {children}
-    </p>
-  );
-}
