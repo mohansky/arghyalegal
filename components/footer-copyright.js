01304df29658fef7581bbec292dee31a6af30559
mohansky <38560439+mohansky@users.noreply.github.com>
2023-08-04 09:05:20 +0530

    Update footer-copyright.js

--- components/footer-copyright.js
+++ components/footer-copyright.js
@@ -3,23 +3,8 @@ import FooterContactData from "@/data/footercontact.json";
 export default function Copyright() {
   return (
     <>
-      <div className="md:flex mx-5 md:mx-10 text-center justify-between text-xs font-thin pb-2">
-        <div>
+      <div className="text-center text-xs font-thin pb-2">
           <p>{FooterContactData.copyright}</p>
-        </div>
-        <div>
-          <p>
-            Designed and Developed by
-            <a
-              className="hover:text-al-primary"
-              href="https://mohankumar.dev/"
-              target="_blank"
-            >
-              {" "}
-              Mohan
-            </a>
-          </p>
-        </div>
       </div>
     </>
   );
